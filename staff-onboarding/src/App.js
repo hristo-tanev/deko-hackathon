0e1392265e97f9ac69dd4d18ce94422c139379de
Hristo Tanev <hristotanev7@gmail.com>
2018-03-23 11:28:20 +0000

    fixed shit

--- staff-onboarding/src/App.js
+++ staff-onboarding/src/App.js
@@ -1,21 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import NewApplicant from './components/new_applicant';
-<<<<<<< HEAD
 import Applicant from './components/applicant';
 import ApplicantContainer from './components/applicant_container';
-import './App.css';
-=======
->>>>>>> 8a177218f1afa2e272574a1d5e1c2498a77a818f
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
         <ApplicantContainer />
-=======
-        <NewApplicant />
->>>>>>> 8a177218f1afa2e272574a1d5e1c2498a77a818f
       </div>
     );
   }
