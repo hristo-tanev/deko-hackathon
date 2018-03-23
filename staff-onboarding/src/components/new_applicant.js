7d575c5c675cd52c73232f7d849e5bf71ae35970
Robinson Coulton <robinsoncoulton@hotmail.co.uk>
2018-03-23 11:05:17 +0000

    added form component for new applicants

---
+++ staff-onboarding/src/components/new_applicant.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+export default class NewApplicant extends Component {
+  render(){
+    return (
+      <form>
+        <label>Name: </label>
+        <input type='text' />
+
+        <label>CV: </label>
+        <input type='text' />
+        <label>Phone: </label>
+
+        <input type='text' />
+        <label>Email: </label>
+        <input type='text' />
+
+        <label>Stages: </label>
+        <input type='text' />
+
+        <button type='submit'>Add! </button>
+      </form>
+      )
+  }
+}
+
+
